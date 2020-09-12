269f2ff001d7f54d94a1bb6f022f86fe0a8bfef3
zainab66 <zainab66r@yahoo.com>
2020-09-12 01:51:36 +0000

    creating urls_index.ejs, filling it out and connected to the server

--- express_server.js
+++ express_server.js
@@ -28,6 +28,15 @@ app.get("/hello", (req, res) => {
 });
 
 
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
