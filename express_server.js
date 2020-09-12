a19bf78f95183b4451e2058c0496a327b553b8d6
zainab66 <zainab66r@yahoo.com>
2020-09-12 02:20:28 +0000

    adding the new urls form as an ejs file and install body-parser|

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.set("view engine", "ejs");
@@ -35,8 +39,20 @@ app.get("/urls", (req, res) => {
   });
 
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
